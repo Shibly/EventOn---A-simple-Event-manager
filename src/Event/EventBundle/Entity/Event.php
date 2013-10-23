51bcab34b85d8d1b92a98783dab72edf8575f245
Shibly <shibly.phy@gmail.com>
2013-10-23 21:39:08 +0600

    Implemented image upload

--- src/Event/EventBundle/Entity/Event.php
+++ src/Event/EventBundle/Entity/Event.php
@@ -2,6 +2,8 @@
 
 namespace Event\EventBundle\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -28,12 +30,6 @@ class Event
      */
     private $name;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="imageName", type="string", length=255, nullable=true)
-     */
-    private $imageName;
 
     /**
      * @var \DateTime
@@ -180,5 +176,91 @@ class Event
     public function getDetails()
     {
         return $this->details;
+    }
+
+
+    //********************************************************File Upload********************************
+
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    public $imageName;
+
+    public function getAbsolutePath()
+    {
+        return null === $this->imageName
+            ? null
+            : $this->getUploadRootDir() . '/' . $this->imageName;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->imageName
+            ? null
+            : $this->getUploadDir() . '/' . $this->imageName;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory imageName where uploaded
+        // documents should be saved
+        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/images';
+    }
+
+    /**
+     * @Assert\File(maxSize="6000000")
+     */
+    private $file;
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and then the
+        // target filename to move to
+        $this->getFile()->move(
+            $this->getUploadRootDir(),
+            $this->getFile()->getClientOriginalName()
+        );
+
+        // set the imageName property to the filename where you've saved the file
+        $this->imageName = $this->getFile()->getClientOriginalName();
+
+        // clean up the file property as you won't need it anymore
+        $this->file = null;
     }
 }
