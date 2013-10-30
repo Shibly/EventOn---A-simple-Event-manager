7b6eb3595bbfe39a597ebecbc8c4cfe87f548ad8
Shibly <shibly.phy@gmail.com>
2013-10-31 00:28:51 +0600

    added more fixtures data

--- src/Event/EventBundle/DataFixtures/ORM/LoadEvents.php
+++ src/Event/EventBundle/DataFixtures/ORM/LoadEvents.php
@@ -42,6 +42,48 @@ class LoadEvents extends AbstractFixture implements ContainerAwareInterface, Ord
         $event2->setOwner($user);
         $manager->persist($event2);
 
+
+        $event3 = new Event();
+        $event3->setName("jQuery meetup !")
+            ->setLocation('Dhaka')
+            ->setTime(new \DateTime('Saturday noon'))
+            ->setDetails("jQuery FTW !");
+        $event3->setOwner($user);
+        $manager->persist($event3);
+
+        $event4 = new Event();
+        $event4->setName("phpXperts")
+            ->setLocation('Dhaka')
+            ->setTime(new \DateTime('Tuesday noon'))
+            ->setDetails("The annual PHP meetup");
+        $event4->setOwner($user);
+        $manager->persist($event4);
+
+        $event5 = new Event();
+        $event5->setName("node.js up and running")
+            ->setLocation('Dhaka')
+            ->setTime(new \DateTime('Thursday noon'))
+            ->setDetails("Rule the web with node.js");
+        $event5->setOwner($user);
+        $manager->persist($event5);
+
+        $event6 = new Event();
+        $event6->setName("Everything about ExpressJS")
+            ->setLocation('Dhaka')
+            ->setTime(new \DateTime('Thursday noon'))
+            ->setDetails("node.js MVC, delivered");
+        $event6->setOwner($user);
+        $manager->persist($event6);
+
+        $event7 = new Event();
+        $event7->setName("Dive into mongodb")
+            ->setLocation('Dhaka')
+            ->setTime(new \DateTime('Thursday noon'))
+            ->setDetails("Dominate big data with nosql !");
+        $event7->setOwner($user);
+        $manager->persist($event7);
+
+
         $manager->flush();
     }
 
