4ce4de1426c2c0b656ab27933e15e5bc423478b7
Shibly <shibly.phy@gmail.com>
2013-11-06 18:07:12 +0600

    Updated the SF2 version and some css

--- src/Event/EventBundle/Controller/DefaultController.php
+++ src/Event/EventBundle/Controller/DefaultController.php
@@ -4,7 +4,8 @@ namespace Event\EventBundle\Controller;
 
 //use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Event\EventBundle\Controller\Controller;
-
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
 class DefaultController extends Controller
 {
     public function indexAction($name)
